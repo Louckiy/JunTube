c6e9aae8084c000847a67e102a469c5956c6fabf
Louckiy <esmelev9@gmail.com>
2025-05-21 17:51:34 +0200

    first commit~

---
+++ main.js
@@ -0,0 +1,21 @@
+let dropdownList = document.querySelector(".dropdown__list")
+
+function changeList() {
+    dropdownList.classList.toggle("active")
+}
+
+let Jun = 1000
+let str = "JunJun"
+let bool = true
+
+Jun = 10
+
+const logoText = document.querySelector(".logo__text")
+//const logoTexts = document.querySelector("")
+
+logoText.innerText = "ffff"
+logoText.style.color = "red"
+
+function changeText() {
+    
+}
